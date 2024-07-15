b89a80dd7bf4aa25a9c2284d781cd00d5758ac70
Jen Dales <dalesjr@mail.uc.edu>
2024-07-14 23:00:57 -0400

    Initial Commit

--- src/CheckOut.java
+++ src/CheckOut.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class CheckOut {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double totalCost = 0.0;
+        boolean moreItems;
+
+        do {
+            double itemCost = SafeInput.getRangedDouble(in, "Please enter the price of the item", 0.50, 10.00);
+            totalCost += itemCost;
+            moreItems = SafeInput.getYNConfirm(in, "Would you like to add more items?");
+        } while (moreItems);
+
+        System.out.printf("\nTotal Cost: $%.2f\n", totalCost);
+    }
 }
