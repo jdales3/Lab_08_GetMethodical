5cf55620b1b80b96215b4e889a8095cd9ab1e41e
Jen Dales <dalesjr@mail.uc.edu>
2024-07-15 09:36:40 -0400

    Commit

--- src/FavNumbers.java
+++ src/FavNumbers.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 
-public class FavNumbers {
-    public static void main(String[] args) {
+public class FavNumbers
+{
+    public static void main(String[] args)
+    {
         Scanner in = new Scanner(System.in);
         int favoriteInt = SafeInput.getInt(in, "Please enter your favorite integer");
         double favoriteDouble = SafeInput.getDouble(in, "Please enter your favorite double");
