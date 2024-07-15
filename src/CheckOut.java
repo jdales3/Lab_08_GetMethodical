b73125d7f6f223ca025d5b055f781497fd911a0c
Jen Dales <dalesjr@mail.uc.edu>
2024-07-15 09:35:33 -0400

    Commit

--- src/CheckOut.java
+++ src/CheckOut.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
 
-public class CheckOut {
-    public static void main(String[] args) {
+public class CheckOut
+{
+    public static void main(String[] args)
+    {
         Scanner in = new Scanner(System.in);
         double totalCost = 0.0;
         boolean moreItems;
 
-        do {
+        do
+        {
             double itemCost = SafeInput.getRangedDouble(in, "Please enter the price of the item", 0.50, 10.00);
             totalCost += itemCost;
             moreItems = SafeInput.getYNConfirm(in, "Would you like to add more items?");
