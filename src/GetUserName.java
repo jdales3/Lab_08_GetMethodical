0b8c4884efdc8a6a79259a5fa93f06be1091ff74
Jen Dales <dalesjr@mail.uc.edu>
2024-07-14 22:52:35 -0400

    Initial Commit

--- src/GetUserName.java
+++ src/GetUserName.java
@@ -1,2 +1,10 @@
+import java.util.Scanner;
+
 public class GetUserName {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String firstName = SafeInput.getNonZeroLenString(in, "Please enter your first name");
+        String lastName = SafeInput.getNonZeroLenString(in, "Please enter your last name");
+        System.out.println("\nYour full name is: " + firstName + " " + lastName);
+    }
 }
