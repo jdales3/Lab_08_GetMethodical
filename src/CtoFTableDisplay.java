4c3e1b6ec0d39fdeb34d94f290d540eabab8a1a5
Jen Dales <dalesjr@mail.uc.edu>
2024-07-14 23:29:20 -0400

    Commit

--- src/CtoFTableDisplay.java
+++ src/CtoFTableDisplay.java
@@ -1,2 +1,19 @@
-public class CtoFTableDisplay {
+public class CtoFTableDisplay
+{
+
+    public static void main(String[] args)
+    {
+        SafeInput.prettyHeader("Celsius to Fahrenheit Conversion Table");
+
+        // Display table headers
+        System.out.printf("%-10s %-10s\n", "Celsius", "Fahrenheit");
+        System.out.println("----------------------------");
+
+        // Display conversion table from -100 to 100 Celsius
+        for (int celsius = -100; celsius <= 100; celsius++)
+        {
+            double fahrenheit = SafeInput.CtoF(celsius);
+            System.out.printf("%-10d %-10.1f\n", celsius, fahrenheit);
+        }
+    }
 }
