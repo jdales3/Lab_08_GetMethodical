6503235238dde849bbe183d2b3576f8fa689c3cc
Jen Dales <dalesjr@mail.uc.edu>
2024-07-14 21:19:35 -0400

    Commit

--- src/SafeInput.java
+++ src/SafeInput.java
@@ -63,4 +63,72 @@ public class SafeInput
         pipe.nextLine(); // clear newline from Scanner buffer
         return value;
     }
+    public static int getRangedInt(Scanner pipe, String prompt, int low, int high)
+    {
+        int value = 0;
+        boolean isValid = false;
+        do
+        {
+            System.out.print("\n" + prompt + " [" + low + " - " + high + "]: ");
+            if (pipe.hasNextInt())
+            {
+                value = pipe.nextInt();
+                if (value >= low && value <= high)
+                {
+                    isValid = true;
+                }
+                else
+                {
+                    System.out.println("Input out of range. Please enter an integer within the specified range.");
+                    pipe.nextLine(); // clear the invalid input
+                }
+            }
+            else
+            {
+                System.out.println("Invalid input. Please enter an integer.");
+                pipe.nextLine(); // clear the invalid input
+            }
+        }
+        while (!isValid);
+        pipe.nextLine(); // clear newline from Scanner buffer
+        return value;
+    }
+    public static double getRangedDouble(Scanner pipe, String prompt, double low, double high)
+    {
+        double value = 0.0;
+        boolean isValid = false;
+        do
+        {
+            System.out.print("\n" + prompt + " [" + low + " - " + high + "]: ");
+            if (pipe.hasNextDouble())
+            {
+                value = pipe.nextDouble();
+                if (value >= low && value <= high)
+                {
+                    isValid = true;
+                }
+                else
+                {
+                    System.out.println("Input out of range. Please enter a double within the specified range.");
+                    pipe.nextLine(); // clear the invalid input
+                }
+            }
+            else
+            {
+                System.out.println("Invalid input. Please enter a double.");
+                pipe.nextLine(); // clear the invalid input
+            }
+        }
+        while (!isValid);
+        pipe.nextLine(); // clear newline from Scanner buffer
+        return value;
+    }
+
+
+
+
+
+}
+
+
 
